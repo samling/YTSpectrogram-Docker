96b7d7b311d13698a116c8ffa150b4b2662e6857
samling <samlingx@gmail.com>
2017-04-19 02:04:42 -0700

    Changing the input from an argument to an environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-hash="$(printf "$1" | cut -d'=' -f2)"
+hash=$YTID
+echo $hash
 
 youtube-dl -x --audio-format flac -o "audio/%(id)s.%(ext)s" $hash
 
 /usr/local/YTSpectrogram/YTSpectrogram $hash
+
