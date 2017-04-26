12515e6da0154c0aac6ce6dce467092609abbd06
samling <samlingx@gmail.com>
2017-04-26 15:51:42 -0700

    Adding test output to container for env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 hash=$YTID
+dbhost=$DB_HOST
+dbuser=$DB_USER
+dbpass=$DB_PASS
+dbname=$DB_NAME
+
 echo $hash
+echo $dbhost
+echo $dbuser
+echo $dbpass
+echo $dbname
 
 youtube-dl -x --audio-format flac -o "audio/%(id)s.%(ext)s" $hash
 
